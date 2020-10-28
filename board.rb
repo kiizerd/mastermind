2831345237477c6eef1b674898eacdbf5633defd
kiizerd <ajsewell13@gmail.com>
2020-10-28 00:58:21 -0500

    made main display all gameplay should show

---
+++ board.rb
@@ -0,0 +1,70 @@
+class Board
+  def initialize
+    @display =   ["\t<[==============]> \n",
+                  "\t | < >< >< >< > | \n\t |   <{    }>   | \n", 
+                  "\t | < >< >< >< > | \n\t |   <{    }>   | \n", 
+                  "\t | < >< >< >< > | \n\t |   <{    }>   | \n",
+                  "\t | < >< >< >< > | \n\t |   <{    }>   | \n",
+                  "\t | < >< >< >< > | \n\t |   <{    }>   | \n", 
+                  "\t | < >< >< >< > | \n\t |   <{    }>   | \n", 
+                  "\t | < >< >< >< > | \n\t |   <{    }>   | \n", 
+                  "\t | < >< >< >< > | \n\t |   <{    }>   | \n", 
+                  "\t | < >< >< >< > | \n\t |   <{    }>   | \n", 
+                  "\t | < >< >< >< > | \n\t |   <{    }>   | \n", 
+                  "\t | < >< >< >< > | \n\t |   <{    }>   | \n", 
+                  "\t | < >< >< >< > | \n\t |   <{    }>   | \n",
+                  "\t<[==============]> \n"]
+  
+  @code_display = ["\t<[-----[==]-----]>", 
+                   "\t | <+><-><*><$> |", 
+                   "\t<[-----[==]-----]>"] 
+  puts @display
+  puts @code_display
+  @counter = 12
+  @code_shown = [false, false, false, false]
+  end
+
+  def show_guess(guess)
+    c = 5
+    guess.each do |i|
+      @display[@counter][c] = i
+      c += 17
+    end
+  end
+  
+  def show_feedback(feedback)
+    c = -12
+    feedback.each do |i|
+      @display[@counter][c] = i
+      c += 1
+    end
+    @counter -= 1
+    puts @display
+    puts @code_display
+  end
+
+  def show_code(num, code)
+    case num
+    when 0
+      if !@code_shown[num]
+        @code_display[1][@code_display[1].index("+")] = code
+        @code_shown[num] = true
+      end
+    when 1
+      if !@code_shown[num]
+        @code_display[1][@code_display[1].index("-")] = code
+        @code_shown[num] = true
+      end
+    when 2
+      if !@code_shown[num]
+        @code_display[1][@code_display[1].index("*")] = code
+        @code_shown[num] = true
+      end
+    when 3
+      if !@code_shown[num]
+        @code_display[1][@code_display[1].index("$")] = code
+        @code_shown[num] = true
+      end
+    end
+  end
+end
